d6bef80b703fe7542c37d8c646b49e8c10e5956c
RoyStorey <imlejiii@gmail.com>
2023-02-10 23:21:33 -0500

    Card done
    Co-authored-by: Brandon <bfreeman12@users.noreply.github.com>

---
+++ js/favorites.js
@@ -0,0 +1,12 @@
+const favoritesBar = document.getElementById('favoritesBar')
+
+
+function favoritesBarActive() {
+  favoritesBar.style.right = '0%'
+  console.log('brandon dumb')
+}
+
+function favoritesBarClosed() {
+  favoritesBar.style.right = '-25%'
+  console.log('roy is stinky')
+}
