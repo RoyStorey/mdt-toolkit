9c14ba42d3abf3f0dc99ccebda9df41789cd279d
bfreeman12 <bfree98@icloud.com>
2023-02-24 14:28:06 -0500

    earl

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 const http = require("http");
 const fs = require("fs");
 const path = require("path");
-const express = require('express')
-require('dotenv').config();
-const hostIp = process.env.HOST_IP
-const apiIP = process.env.API_IP
+const express = require("express");
+require("dotenv").config();
+const hostIp = process.env.HOST_IP;
+const apiIP = process.env.API_IP;
 const hostname = hostIp;
 const port = 3000;
 
 const app = express();
-app.use(express.static('public'));
-app.get('/',(req,res) =>{
-  res.sendFile(path.join(__dirname,'public','index.html'));
-})
+app.use(express.static("LiveEarlReaction"));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "LiveEarlReaction/public", "index.html"));
+});
 app.get("/data", (req, res) => {
   res.json({ value: apiIP });
 });
-app.listen(port,() => console.log(`server ${hostname} started on port ${port}`));
+app.listen(port, () =>
+  console.log(`server ${hostname} started on port ${port}`)
+);
